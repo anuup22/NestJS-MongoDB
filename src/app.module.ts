df9be4dfbe964d9574160c9c111d54ea22a7d88b
Anup Gupta <anuup22.cs@gmail.com>
2024-11-11 23:33:27 +0530

    add mongoose and users module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:2211@cluster0.pbd3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
